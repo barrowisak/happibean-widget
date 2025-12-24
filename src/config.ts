fd6ffaf3df376b178d2e51fe2b7a07e1d6e506e4
Isak <barrowisak@gmail.com>
2025-12-24 02:01:13 +0100

    Update widget design from portal

--- src/config.ts
+++ src/config.ts
@@ -29,7 +29,7 @@ const defaultConfig: HappiBeanConfig = {
     secondary: '#8B7355',
     background: '#FFFFFF'
   },
-  tabs: ["home","messages","help","contact"],
+  tabs: ["messages","home","help","contact"],
   texts: {
     welcomeTitle: 'Welcome!',
     welcomeSubtitle: 'How can we help you today?'
